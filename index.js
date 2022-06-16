3103b9a5cd1c5724ac146effd295302000f7a9f3
venu <bhupathiraju1998@gmail.com>
2022-06-16 22:24:51 +0530

    select your pet

---
+++ index.js
@@ -0,0 +1,13 @@
+let petsImageUrls = {
+    dog: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-dog-img.png",
+    cat: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-cat-img.png",
+    parrot: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-parrot-img.png",
+    spider: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-spider-img.png",
+    rabbit: "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/select-your-pet-rabbit-img.png"
+};
+let petEl = document.getElementById("petSelect");
+let petImgEl = document.getElementById("petImg");
+petEl.addEventListener("change", function(event) {
+    let val = event.target.value;
+    petImgEl.src = petsImageUrls[val];
+});
